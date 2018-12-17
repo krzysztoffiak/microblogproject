15ccdf1094eda1e9317f12a67740ed1b288d77da
unknown <michal.krysztofiak@gmail.com>
2018-12-17 19:53:13 +0100

    my first repository

---
+++ src/main/java/pl/com/sda/javagda17/microblogproject/MicroblogprojectApplication.java
@@ -0,0 +1,14 @@
+package pl.com.sda.javagda17.microblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroblogprojectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/com/sda/javagda17/microblogproject/MicroblogprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.com.sda.javagda17.microblogproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroblogprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
